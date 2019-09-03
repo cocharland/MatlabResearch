14b5f94eaf538d42bb4d5d394a5460cf0434a103
charland <charland.cody@gmail.com>
2019-09-03 12:20:12 -0600

    adding hough trnasforms

--- particle_filter.m
+++ particle_filter.m
@@ -23,7 +23,6 @@ function [chi_t] = particle_filter(chi_t_minus_1, u_t, z_t)
     end
     total = sum(weight);
     weight = weight./total;
-    %plot(weight,'*')
     newParticles = randsample(numParticles,numParticles,true,weight);
     tmpMap = currentParticle.physicalMap;
     %tmpMap = zeros(currentParticle.height,currentParticle.width);
@@ -32,14 +31,29 @@ function [chi_t] = particle_filter(chi_t_minus_1, u_t, z_t)
 %         for k = 1:currentParticle.width
     x = currentParticle.robotPose(1);
     y = currentParticle.robotPose(2);
-    for j = (y-2):y+2
-        for k = (x-2):(x+2)
-            tmpMap(j,k) = sum(localMapSet(j,k,newParticles))/numParticles;
-            chi_t.physicalMap(j,k) = log(tmpMap(j,k)/(1-tmpMap(j,k)));
-%             if chi_t.physicalMap(j,k) == Inf || chi_t.physicalMap(j,k) == -Inf
-%                 chi_t.physicalMap(j,k) = 0;
-%             end
+    theta = currentParticle.robotPose(3);
+    if theta == 1
+        xObs = x +  currentParticle.observationXVector;
+        yObs = y +  currentParticle.observationYVector;
+    elseif theta == 2
+        xObs = x + currentParticle.observationYVector;
+        yObs = y + currentParticle.observationXVector;
+    elseif theta == 3
+        xObs = x + currentParticle.observationXVector;
+        yObs = y - currentParticle.observationYVector;
+    else
+        xObs = x - currentParticle.observationYVector;
+        yObs = y + currentParticle.observationXVector;
+    end
+
+    for j = 1:length(xObs)
+        row = yObs(j);
+        col = xObs(j);
+        if (row <= 0 || row > currentParticle.height || col <= 0 || col > currentParticle.width)
+            continue;
         end
+        tmpMap(row,col) = sum(localMapSet(row,col,newParticles))/numParticles;
+        chi_t.physicalMap(row,col) = log(tmpMap(row,col)/(1-tmpMap(row,col)));
     end
 
 end
